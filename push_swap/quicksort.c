6881099ae4406352fa8b31f4f77549198372558e
Mano Ségransan <msegrans@student.42lausanne.ch>
2021-11-12 17:30:04 +0100

    Push swap optimisations, need to do functions for stack <= 5

--- push_swap/quicksort.c
+++ push_swap/quicksort.c
@@ -109,65 +109,94 @@ int get_closest_index(t_list *lst, int *sorted, int start, int end)
 	return (mindex);
 }
 
+void put_stack(t_list **head_a, t_list **head_b, int lindex, int lstsize)
+{
+	if (lindex <= lstsize / 2)
+	{
+		while (lindex > 0)
+		{
+			ft_printf("ra\n");
+			rotate_stack(head_a);
+			lindex--;
+		}
+	}
+	else
+	{
+		while (lindex != lstsize)
+		{
+			ft_printf("rra\n");
+			rotate_reverse_stack(head_a);
+			lindex++;
+		}
+	}
+	ft_printf("pb\n");
+	push_stack(head_a, head_b);
+}
+
+int get_interval(t_list *lst, int n, int *split, int *sorted)
+{
+	int i;
+	int minindex;
+
+	i = 0;
+	minindex = -1;
+	while (minindex == -1)
+		minindex = ft_lstfind(lst, sorted[i++]);
+	i--;
+	if (i + *split > n)
+		*split = n - i;
+	return (i);
+}
+
 void split(t_list **head_a, t_list **head_b)
 {
 	int n;
-	int div = 6;
+	int div = 4;
 	int i;
 	int j;
+	int k;
+	int u;
 	int lindex;
 	int lstsize;
 	int *sorted;
 
 	n = ft_lstsize(*head_a);
+	int split = n / div;
 	sorted = get_sorted(*head_a, n);
 	i = -1;
-	while (++i < div)
+	while (++i <= div)
 	{
 		j = -1;
 		while (++j < n / div)
 		{
-//			ft_printf("%d-%d (%d) (%d) --> %d\n", i * (n / 10), (i + 1) * (n / 10), ft_lstsize(*head_a), ft_lstsize(*head_b), n);
 			lstsize = ft_lstsize(*head_a);
-			if ((i + 1) * (n / div) > n + 1)
-				lindex = get_closest_index(*head_a, sorted, i * (n / div), n + 1);
-			else
-				lindex = get_closest_index(*head_a, sorted, i * (n / div), (i + 1) * (n / div));
-			if (lindex <= lstsize / 2)
+			if (lstsize < 1)
+				break;
+			k = get_interval(*head_a, n, &split, sorted);
+//			ft_printf("%d-%d-%d (%d) (%d) --> %d\n", k, k + split / 2, k + split, ft_lstsize(*head_a), ft_lstsize(*head_b), n);
+			lindex = get_closest_index(*head_a, sorted, k, k + split / 2);
+			u = get_closest_index(*head_a, sorted, k + split / 2, k + split);
+//			ft_printf("(%d) (%d)\n", lindex, u);
+			if (u != -1 && (lindex == -1 || min(lindex, lstsize - lindex) > min(u, lstsize - u)))
 			{
-				while (lindex > 0)
-				{
-					ft_printf("ra\n");
-					rotate_stack(head_a);
-					lindex--;
-				}
-			}
-			else
-			{
-				while (lindex != lstsize)
-				{
-					ft_printf("rra\n");
-					rotate_reverse_stack(head_a);
-					lindex++;
-				}
-			}
-			if (*head_b && *head_a && (*head_a)->content > (*head_b)->content)
-			{
+				put_stack(head_a, head_b, u, lstsize);
 				ft_printf("rb\n");
 				rotate_stack(head_b);
 			}
-			ft_printf("pb\n");
-			push_stack(head_a, head_b);
-
+			else
+			{
+				put_stack(head_a, head_b, lindex, lstsize);
+			}
+		}
 //			ft_printf("Printing stack a:\n");
 //			print_stack(*head_a);
 //			ft_printf("\n\n");
 //			ft_printf("Printing stack b:\n");
 //			print_stack(*head_b);
 //			ft_printf("\n\n");
-		}
 	}
 }
+
 
 void	solve(t_list **head_a, t_list **head_b)
 {
@@ -206,10 +235,86 @@ void	solve(t_list **head_a, t_list **head_b)
 	}
 }
 
+void clone_lst(t_list *lst, t_list **clone);
+
+int lstmin(t_list *lst)
+{
+	int i;
+	int j;
+	int min;
+
+	i = 0;
+	j = 0;
+
+	min = lst->content;
+	while (lst)
+	{
+		if (lst->content < min)
+		{
+			min = lst->content;
+			j = i;
+		}
+		i++;
+		lst = lst->next;
+	}
+	return (j);
+}
+
+int lstmax(t_list *lst)
+{
+	int i;
+	int j;
+	int max;
+	i = 0;
+	j = 0;
+	max = lst->content;
+	while (lst)
+	{
+		if (lst->content > max)
+		{
+			max = lst->content;
+			j = i;
+		}
+		i++;
+		lst = lst->next;
+	}
+	return (j);
+}
+
+void brute_force3(t_list *lst)
+{
+	if (lstmin(lst) == 0)
+	{
+		ft_printf("sa\n");
+		ft_printf("ra\n");
+	}
+	else if (lstmin(lst) == 1)
+	{
+		if (lstmax(lst) == 0)
+			ft_printf("ra\n");
+		else
+			ft_printf("sa\n");
+	}
+	else
+	{
+		if (lstmax(lst) == 1)
+			ft_printf("rra\n");
+		else
+		{
+			ft_printf("sa\n");
+			ft_printf("rra\n");
+		}
+	}
+}
+
 int sort(t_list **head_a, t_list **head_b, int n)
 {
+	if (is_sorted(*head_a, n))
+		return (1);
 	if (n <= 3)
-	{}
+	{
+		brute_force3(*head_a);
+	}
 	if (n <= 5)
 	{}
 	else
