a692b9f542e653c1a8ecb96576a0c97669d54db9
Mano Ségransan <msegrans@student.42lausanne.ch>
2022-05-06 14:48:51 +0200

    Add template for ft_containers

---
+++ ft_containers/src/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Mano Segransan on 5/5/22.
+//
---
+++ ft_containers/src/vector.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Mano Segransan on 5/5/22.
+//
+
+#ifndef FT_CONTAINERS_VECTOR_HPP
+#define FT_CONTAINERS_VECTOR_HPP
+
+namespace ft
+{
+  class vector
+  {
+
+  };
+}
+
+
+#endif //FT_CONTAINERS_VECTOR_HPP
